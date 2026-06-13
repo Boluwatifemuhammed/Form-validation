d1539d0b1a3dd7f291de0992f29ff24bc34d2b9a
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-06-13 13:28:22 +0100

    updated

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const email  = document.getElementById("email");
 const repeatPassword  = document.getElementById("repeat-password");
 
 Form.addEventListener("submit", function (e) {
-    e.preventDefault
+    e.preventDefault();
+    
+    const passwordValue = password.value;
     
 })
